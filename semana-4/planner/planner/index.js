8437a60c347973557a31d4e858c83350d62cf7c4
SarahHessel <sarah.phessel@gmail.com>
2020-10-06 21:45:27 -0300

    Atividade obrigatória + Desafio 1

--- semana-4/planner/planner/index.js
+++ semana-4/planner/planner/index.js
@@ -1,10 +1,35 @@
 function criarTarefa () {
-    const inputTarefa = document.getElementById("tarefa")
-    const diasDaSemana = document.getElementById("dias-semana")
-    const listaTarefas = document.getElementById(escolherDia)
+    const inputTarefa = document.getElementById("tarefa") 
+    const selectDias = document.getElementById("dias-semana")
 
-    if(inputTarefa !==  ""){
-        listaTarefas.innerHTML += `<li>${inputTarefa.value}</li>`
-        inputTarefa.value =""
+    if(inputTarefa.value === ""){   // Se a caixa de texto estiver vazia, apresenta um alerta para o usuario e retorna.
+        alert("Escreva algo antes de apertar o botão :D")
+        return
     }
+    console.log(inputTarefa.value)  // Imprime no console do navegador cada value inserido.
+
+    switch(selectDias.value){
+        case "domingo":
+            document.getElementById("domingo").innerHTML += `<li>${inputTarefa.value}</li>`
+            break
+        case "segunda":
+            document.getElementById("segunda").innerHTML += `<li>${inputTarefa.value}</li>`
+            break
+        case "terca":
+            document.getElementById("terca").innerHTML += `<li>${inputTarefa.value}</li>`
+            break
+        case "quarta":
+            document.getElementById("quarta").innerHTML += `<li>${inputTarefa.value}</li>`
+            break
+        case "quinta":
+            document.getElementById("quinta").innerHTML += `<li>${inputTarefa.value}</li>`
+            break
+        case "sexta":
+            document.getElementById("sexta").innerHTML += `<li>${inputTarefa.value}</li>`
+            break
+        case "sabado":
+            document.getElementById("sabado").innerHTML += `<li>${inputTarefa.value}</li>`
+            break    
+    }
+    inputTarefa.value = "" // Essa linha serve para limpar o value do input, após cada adição na lista.
 }
