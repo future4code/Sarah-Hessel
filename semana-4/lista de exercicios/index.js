d302e971864fe99d6f5f29e53748477c6bc07e49
SarahHessel <sarah.phessel@gmail.com>
2020-10-13 05:45:34 -0300

    Exercícios de interpretação de código

---
+++ semana-4/lista de exercicios/index.js
@@ -0,0 +1,29 @@
+// Exercícios de interpretação
+
+/* 
+Ex. 1 
+
+    Na 1º linha está uma função conversorDeMoeda que recebe como parametro valorEmDolar, dentro desta função existe 
+uma constante cotacao que imprime uma mensagem na tela e recebe um valor em Number que o usuario digitará. 
+No final da função é retornado uma string concatenado ao valorEmDolar multiplicado (*) por cotacao. 
+    Fora da função é declarado uma constante meuDinheiro que recebe a função conversorDeMoeda e um parametro de valor qualquer, por exemplo: (100)
+logo abaixo é acionado ao programa para imprimir na tela através do console.log a constante meuDinheiro.
+
+exemplo: cotacao = 5,52 reais
+
+--- saida: 552,48 ---
+
+
+Ex.2
+
+    Na 1º linha é declarada uma função investeDinheiro que recebe dois parametros tipoDeInvestimento e valor,
+dentro desta função é declarada uma variavel valorAposInvestimento e logo abaixo um switch.
+O switch irá guardar as strings do tipoDeInvestimento em cases com um resultante valorPosInvestimento referente a cada tipo. 
+    Fora do switch é retornado o valorPosInvestimento do switch e fora da função é declarado duas variáveis novoMontente e segundoMontante 
+que recebem de atribuição a invocação da função investeDinheiro, logo após é chamado dois console.log e as variáveis são impressas.
+
+Ex. 3 
+
+    
+
+*/
