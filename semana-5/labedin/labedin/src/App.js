582e46cf9d561c66719607e708ee43426bf665e5
SarahHessel <sarah.phessel@gmail.com>
2020-10-14 22:20:13 -0300

    LabedIn com react

---
+++ semana-5/labedin/labedin/src/App.js
@@ -0,0 +1,70 @@
+import React from 'react';
+import './App.css';
+import CardGrande from './components/CardGrande/CardGrande';
+import ImagemButton from './components/ImagemButton/ImagemButton';
+import CardPequeno from './components/CardPequeno/CardPequeno';
+import fotinha from './bobesponja.jpg';
+import email from './email.png'
+import local from './localizacao.png'
+
+function App() {
+  return (
+    <div className="App">
+      <div className="page-section-container">
+        <h2>Dados pessoais</h2>
+        <CardGrande 
+          imagem={fotinha} 
+          nome="Sarah" 
+          descricao="Oi, eu sou a Sarah. Sou aluna da Labenu, faço o curso de Desenvolvimento Web Full Stack, turma Dumont! :)"
+        />
+        
+        <ImagemButton 
+          imagem="https://image.flaticon.com/icons/png/512/117/117472.png" 
+          texto="Ver mais"
+        />
+      </div>
+      <div className="page-section-container">
+        <CardPequeno
+          imagem={email}
+          nome="Email:"
+          descricao=" sarah.estudante@Labenu.com"
+        />
+         <CardPequeno
+          imagem={local}
+          nome="Endereço:"
+          descricao=" Cidade das Conchas, 299"
+        />
+      </div>
+
+      <div className="page-section-container">
+        <h2>Formação </h2>
+        <CardGrande 
+          imagem="https://s3.amazonaws.com/future4.com.br/static/headf4-c492117ca2373dc85ca81bf715b3dc2a.png" 
+          nome="Labenu" 
+          descricao="Módulo 2/6 em Desenvolvimento Web Full Stack." 
+        />
+        
+        <CardGrande 
+          imagem="https://imagens.canaltech.com.br/empresas/4418.400.jpg" 
+          nome="NASA" 
+          descricao="Apontando defeitos." 
+        />
+      </div>
+      
+      <div className="page-section-container">
+        <h2>Minhas redes sociais</h2>
+        <ImagemButton 
+          imagem="https://d2v9ipibika81v.cloudfront.net/uploads/sites/261/2017/01/facebook-logo-3.png" 
+          texto="Facebook" 
+        />        
+
+        <ImagemButton 
+          imagem="https://logodownload.org/wp-content/uploads/2014/09/twitter-logo-1-1.png" 
+          texto="Twitter" 
+        />        
+      </div>
+    </div>
+  );
+}
+
+export default App;
