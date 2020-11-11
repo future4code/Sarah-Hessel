422d85eb94d651b2d3d9fc87d8689b7988fa5761
SarahHessel <sarah.phessel@gmail.com>
2020-11-11 16:47:01 -0300

    AstroMatch

---
+++ semana-9/AstroMatch/astromatch/src/components/Buttons/Buttons.js
@@ -0,0 +1,7 @@
+import React, { useState } from 'react'
+
+export default function Buttons() {
+    return (
+        <div>Aqui fica os botões</div>
+    )
+}
---
+++ semana-9/AstroMatch/astromatch/src/components/ListMatch/ListMatch.js
@@ -0,0 +1,7 @@
+import React, { useState } from 'react'
+
+export default function ListMatch() {
+    return (
+        <div>Aqui fica a lista de matches</div>
+    )
+}
