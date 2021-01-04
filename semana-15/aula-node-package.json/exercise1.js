2cb64b861435e5d46a562d53db2b887c6e817217
SarahHessel <sarah.phessel@gmail.com>
2021-01-04 15:44:44 -0300

    Anotações exercicio 1

--- semana-15/aula-node-package.json/exercise1.js
+++ semana-15/aula-node-package.json/exercise1.js
@@ -1,5 +1,10 @@
 
-const name = process.argv[2]
+// a) Para acessar os parametrôs, atribuimos a uma variavel o argumento com um valor de indice especifico.
+
+// node caminho-para-o-arquivo terceiro-argumento quarto-argumento       // caminho de acesso no terminal 
+// [0]          [1]                 [2]                 [3]
+
+const name = process.argv[2] // Argumentos passados no terminal para execução do programa
 const age = process.argv[3]
 
 const addSeven = Number(age) + 7
