e7714cd52c4764317f1a49ce39be47cb6ed227d2
SarahHessel <sarah.phessel@gmail.com>
2021-01-04 15:36:29 -0300

    Exercicio 1

---
+++ semana-15/aula-node-package.json/exercise1.js
@@ -0,0 +1,7 @@
+
+const name = process.argv[2]
+const age = process.argv[3]
+
+const addSeven = Number(age) + 7
+
+console.log(`Olá, ${name}! Você tem ${age} anos. Daqui 7 anos você terá ${addSeven} anos `)
